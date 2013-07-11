7e1571a2b2f4c2424be130171466ebd0b0cc1190
Leonardo Luarte <leonardo@luarte.net>
2013-07-11 13:36:26 -0400

    mend

--- app/serializers/plan_serializer.rb
+++
@@ -1,4 +0,0 @@
-class PlanSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  has_one :product
-end
--- test/controllers/plans_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PlansControllerTest < ActionController::TestCase
-end
